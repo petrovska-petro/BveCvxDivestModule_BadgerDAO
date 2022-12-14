cf3597b52876caa26102c79507e961f37c5a8216
Petrovska <petrovska@badger.finance>
2022-12-15 01:16:15 +0530

    feat: include tests for setters of `setWithdrawableFactor` & `setWeeklyCvxSpotAmount`

--- tests/test_gov_priviliages.py
+++ tests/test_gov_priviliages.py
@@ -31,3 +31,34 @@ def test_set_guardian(bvecvx_module, governance, accounts):
 def test_set_guardian_random_account(bvecvx_module, accounts):
     with reverts("not-governance!"):
         bvecvx_module.setGuardian(accounts[3], {"from": accounts[6]})
+
+
+def test_set_withdrawable_factor(bvecvx_module, governance, techops):
+    bvecvx_module.setWithdrawableFactor(5_000, {"from": governance})
+    assert bvecvx_module.factorWd() == 5_000
+
+    bvecvx_module.setWithdrawableFactor(6_000, {"from": techops})
+    assert bvecvx_module.factorWd() == 6_000
+
+
+def test_set_withdrawable_factor_greater_than_max(bvecvx_module, governance):
+    with reverts(">MAX_FACTOR_WD!"):
+        bvecvx_module.setWithdrawableFactor(9_000, {"from": governance})
+
+
+def test_set_withdrawable_factor_random_account(bvecvx_module, accounts):
+    with reverts("not-gov-or-guardian"):
+        bvecvx_module.setWithdrawableFactor(5_000, {"from": accounts[6]})
+
+
+def test_set_weekly_cvx_spot_amount(bvecvx_module, governance, techops):
+    bvecvx_module.setWeeklyCvxSpotAmount(15_000, {"from": governance})
+    assert bvecvx_module.weeklyCvxSpotAmount() == 15_000
+
+    bvecvx_module.setWeeklyCvxSpotAmount(3_000, {"from": techops})
+    assert bvecvx_module.weeklyCvxSpotAmount() == 3_000
+
+
+def test_set_weekly_cvx_spot_amount_random_account(bvecvx_module, accounts):
+    with reverts("not-gov-or-guardian"):
+        bvecvx_module.setWeeklyCvxSpotAmount(5_000, {"from": accounts[6]})
